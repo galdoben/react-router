850a17ce40618cc51f83e08cbad5a75dff8bd394
galdoben <galdobenedetta@gmail.com>
2025-01-24 16:00:31 +0100

    fix: btn-back

--- src/components/CardPost.jsx
+++ src/components/CardPost.jsx
@@ -29,6 +29,9 @@ const CardPost = () => {
                     <h5 className="card-title">{post?.title}</h5>
                     <p className="card-text">{post?.content}</p>
                     <a href="#" className="btn btn-primary">Elimina</a>
+                    <div className="d-flex justify-content-between">
+                        <button type="button" className="btn btn-warning" onClick={() => navigate(-1)}>Back</button>
+                    </div>
                 </div>
             </div>
         </div>
